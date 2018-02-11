32ad73768e3a33d025e35555cc7d1ddf39ce01c6
Tomkc518 <32521131+Tomkc518@users.noreply.github.com>
2018-02-10 19:14:53 -0600

    final updates

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# week-4-game
-Star Wars Lightsaber Battle!
+# Star Wars Lightsaber Battle!
+
+This project was super challenging.  It pushed my knowledge to the limit. The logic got pretty intense at times but it wall worked out in the end!
+
+The biggest thing I learned was about maintaining the state of the game. Knowing which character was chosen and who were the enemies and who was the defender.
+
+You can choose any of the characters as who you will play through the whole game.
+
+Then all of the other characters are your enemies.
+
+Of those, you must choose in which order to defeat the enemies.
+
+Once all of them are defeated you win! Otherwise when your hp gets to <=0 you lose :(
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,115 +1,107 @@
 $(function() {
-    var characterSelected = true;
-    var enemySelected = true;
-    var characterSet = true;
-    var enemySet = true;
-    var character = "";
-    var enemy = "";
-    
-    var obiWan = {
-        id: "1",
-        health: 120,
-        attack: 10,
-        counterAttack: 15,
-    };
-    var luke = {
-        id: "2",
-        health: 100,
-        attack: 8,
-        counterAttack: 12,
-    };
-    var darthSid = {
-        id: "3",
-        health: 150,
-        attack: 10,
-        counterAttack: 20,
-    };
-    var darthMaul = {
-        id: "4",
-        health: 180,
-        attack: 12,
-        counterAttack: 25,
-    };
+    var characterSelected = false;
+    var enemySelected = false;
+    var enemiesDefeated = 0;
+    var instructions = $('#instructions');
+    var characterCombatStatus = $("#characterCombat");
+    var enemyCombatStatus = $("#enemyCombat");
 
-    var possibleCharacters = [obiWan, luke, darthSid, darthMaul];
+    $(instructions).html("<h2>Please choose a character</h2>");
+
+    var characters = [
+        {
+            name: "Obi-Wan Kenobi",
+            health: 120,
+            baseAttack: 10,
+            counterAttack: 15
+        },
+        {
+            name: "Luke SKywalker",
+            health: 100,
+            baseAttack: 8,
+            counterAttack: 12
+        },
+        {
+            name: "Darth Sidius",
+            health: 170,
+            baseAttack: 10,
+            counterAttack: 20
+        },
+        {
+            name: "Darth Maul",
+            health: 180,
+            baseAttack: 12,
+            counterAttack: 25
+        }
+    ];
+
     // When the game starts, the player will choose a character by clicking on the fighter's picture. The player will fight as that character for the rest of the game.
     $(".characterBox").on("click", function() {
-        if (characterSelected) {
+        if (!characterSelected) {
             $(".characterRow").append($(this));
             $(this).removeClass("characterBox")
-            characterSelected = false;
-            character = $(this).attr("id");
+            characterSelected = characters[$(this).attr("id")];
+            characterSelected.element = $(this);
+            characterSelected.currentAttack = characterSelected.baseAttack;
             // The player must then defeat all of the remaining fighters. Enemies should be moved to a different area of the screen.
             $(".characterBox").each(function() {
                 $(".attackRow").append($(this));
                 $(this).addClass("enemiesToAttack");
                 $(this).css({"background-color": "red", "border": "1px solid black"});
             });
+            $(instructions).html("<h2>Please choose an enemy</h2>");
         };
     });
+
     // The player chooses an opponent by clicking on an enemy's picture.
     $(document).on('click', ".enemiesToAttack", function() {
         // Once the player selects an opponent, that enemy is moved to a defender area.
-        if (enemySelected) {
+        if (!enemySelected) {
             $(".defenderRow").append($(this));
             $(this).css({"background-color": "black", "border": "1px solid green", "color": "white"});
-            enemySelected = false;
-            enemy = $(this).attr("id");
+            enemySelected = characters[$(this).attr("id")];
+            enemySelected.element = $(this);
+            $(instructions).html("<h2>Click attack to begin combat.</h2>");
         };
     });
+
     // The player will now be able to click the attack button.
     $(".attackButton").on("click", function() {
-        if (characterSelected === false && enemySelected === false) {
+        if (characterSelected && enemySelected) {
             // Whenever the player clicks attack, their character damages the defender. The opponent will lose HP (health points). These points are displayed at the bottom of the defender's picture.
-            if (characterSet){
-                for (var i = 0; i < possibleCharacters.length; i++) {
-                    if (character === possibleCharacters[i].id) {
-                        characterAttack = possibleCharacters[i].attack;
-                        characterAttackIncrease = possibleCharacters[i].attack;
-                        characterHealth = possibleCharacters[i].health;
-                        characterSet = false;
-                    };
-                };
-            };
-             // The opponent character will instantly counter the attack. When that happens, the player's character will lose some of their HP. These points are shown at the bottom of the player character's picture.
-            if (enemySet){
-                for (var j = 0; j < possibleCharacters.length; j++) {
-                    if (enemy === possibleCharacters[j].id) {
-                        enemyAttack = possibleCharacters[j].attack;
-                        enemyHealth = possibleCharacters[j].health;
-                        enemySet = false;
-                    };
-                };
-            };
             
             // The player will keep hitting the attack button in an effort to defeat their opponent.
-            characterHealth -= enemyAttack;
-            enemyHealth -= characterAttack;
-            characterAttack += characterAttackIncrease;
+            characterSelected.health -= enemySelected.counterAttack;
+            enemySelected.health -= characterSelected.currentAttack;
+            characterSelected.currentAttack += characterSelected.baseAttack;
                         
-            $("#" + character + "> .imageTextBottom").text(characterHealth);
-            $("#" + enemy + "> .imageTextBottom").text(enemyHealth);
-        
-            
+            // Display health changes
+            var characterHealthBox = $(characterSelected.element).find(".imageTextBottom")[0];
+            var enemyHealthBox = $(enemySelected.element).find(".imageTextBottom")[0];
+
+            $(characterHealthBox).html(characterSelected.health);
+            $(enemyHealthBox).html(enemySelected.health);
+
+            $(characterCombatStatus).html("<p>You attacked " + enemySelected.name + " for " + characterSelected.currentAttack + " damage.</p>");
+            $(enemyCombatStatus).html("<p>" + enemySelected.name + " attacked you back for  " + enemySelected.counterAttack + " damage.</p>");
+
+            // When the defender's HP is reduced to zero or below, remove the enemy from the defender area. The player character can now choose a new opponent.
+            if (enemySelected.health <= 0) {
+                $(enemySelected.element).remove();
+                enemiesDefeated += 1;
+                // When all defender's HP is reduced to zero or below, win the game
+                if (enemiesDefeated >= (characters.length - 1)) {
+                    $(instructions).html("<h2>" + enemySelected.name + " was defeated! You have defeated all of the enemies!</h2>");
+                } else {
+                    $(instructions).html("<h2>" + enemySelected.name + " was defeated! Choose a new enemy.</h2>");
+                }
+                enemySelected = false;
+                // When your HP is reduced to zero or below, lose the game
+            } else if (characterSelected.health <= 0) {
+                $(instructions).html("<h2>" + characterSelected.name + " was defeated.</h2>");
+                characterSelected = false;
+            }
         };
     });
     
 });
-
-
-
-
-// When the defender's HP is reduced to zero or below, remove the enemy from the defender area. The player character can now choose a new opponent.
-
-// The player wins the game by defeating all enemy characters. The player loses the game the game if their character's HP falls to zero or below.
-
-
-
-
-
-
-
-
-
-
-
